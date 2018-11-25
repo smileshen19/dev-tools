b5168c2c2ae4b4d919098a6981aefbab9ae346c0
shermanshen19 <shermanshen@digiwin.biz>
2018-11-25 14:41:14 +0800

    adjust

--- src/app/providers/node-child-process.service.ts
+++ src/app/providers/node-child-process.service.ts
@@ -7,23 +7,20 @@ export class NodeChildProcessService {
 
     constructor() { }
 
-    executeCommand(command: string): Observable<string> {
+    executeCommand(command: string): Observable<{ stdout: string, stderr: string }> {
         console.log('command = ', command);
-        const ob = new Observable<string>((subscriber: Subscriber<string>) => {
-            ElectronRendererAPI.childProcess.exec(command, function (error, stdout, stderr) {
+        const ob = new Observable<{ stdout: string, stderr: string }>((subscriber: Subscriber<{ stdout: string, stderr: string }>) => {
+            ElectronRendererAPI.childProcess.exec(command, function (error: string, stdout: string, stderr: string) {
                 console.log('error = ', error);
                 console.log('stdout = ', stdout);
                 console.log('stderr = ', stderr);
-                subscriber.next();
-                subscriber.complete();
-                // if (error) {
-                //     console.error('err = ', error);
-                //     subscriber.error(error);
-                // } else {
-                //     console.log('data.toString() = ', data.toString());
-                //     subscriber.next(data.toString());
-                //     subscriber.complete();
-                // }
+                if (!error) {
+                    subscriber.next({ stdout: stdout, stderr: stderr });
+                    subscriber.complete();
+                } else {
+                    console.error('NodeChildProcessService executeCommand() error = ', error);
+                    subscriber.error(error);
+                }
             });
         });
         return ob;
